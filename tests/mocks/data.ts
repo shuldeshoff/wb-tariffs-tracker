d2c660bedb717723aff4cf04a9e1df54731e96c6
shuldeshoff <shuldeshoff@mail.ru>
2025-10-22 23:32:23 +0300

    Update README: remove emojis, add author info; cleanup docs

---
+++ tests/mocks/data.ts
@@ -0,0 +1,20 @@
+export const mockWBApiResponse = {
+    response: {
+        data: {
+            dtNextBox: "2025-10-23T10:00:00Z",
+            dtTillMax: "2025-10-30T10:00:00Z",
+            warehouseList: [
+                {
+                    warehouseName: "Коледино",
+                    boxTypeName: "Короб",
+                    boxDeliveryAndStorageExpr: "1.5",
+                    boxDeliveryBase: "50",
+                    boxDeliveryLiter: "10",
+                    boxStorageBase: "30",
+                    boxStorageLiter: "5",
+                },
+            ],
+        },
+    },
+};
+
